7fd8f0966001ca4aa9b57129026faacc82f4ac56
Ram Koushik Reddy <ramkoushikreddy@Rams-MacBook-Pro.local>
2023-11-29 17:05:19 +0000

    Excelsheet and some changes

--- src/test/java/HRPortalLoginTest.java
+++ src/test/java/HRPortalLoginTest.java
@@ -5,22 +5,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 public class HRPortalLoginTest {
+    private static final String EXCEL_PATH = "/Users/ramkoushikreddy/IdeaProjects/ECSAutomation/ExcelData/testDataLogin.xlsx";
+    private static final String LOGIN_URL = "http://ecs.memss.co.uk";
+    private static final String EXPECTED_TITLE = "Dashboard | PixelsHR - All in One Cloud HR Software";
+
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         try {
-            List<String[]> loginDataList = ExcelUtility.getAllLoginData("/Users/ramkoushikreddy/Downloads/testDataLogin.xlsx");
+            List<String[]> loginDataList = ExcelUtility.getAllLoginData(EXCEL_PATH);
 
             for (String[] loginData : loginDataList) {
-                driver.get("http://ecs.memss.co.uk"); // Replace with actual URL
+                driver.get(LOGIN_URL);
 
                 LoginPage loginPage = new LoginPage(driver);
                 loginPage.setUsername(loginData[0]); // Set username
                 loginPage.setPassword(loginData[1]); // Set password
                 loginPage.clickLogin();
 
-                // Add validation and a brief wait here if necessary
+                // Validate login by checking the page title
+                if (driver.getTitle().equals(EXPECTED_TITLE)) {
+                    System.out.println("Login successful for user: " + loginData[0]);
+                } else {
+                    System.out.println("Login failed for user: " + loginData[0]);
+                }
             }
 
         } catch (Exception e) {
