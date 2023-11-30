1a87458f8503a14ed263140f080606a48f944946
Ram Koushik Reddy <ramkoushikreddy@Rams-MacBook-Pro.local>
2023-11-30 16:10:50 +0000

    TestNG framework implementation

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,5 @@ L
 src/test/java/LoginPage.java,b/c/bc2180297945c14b7cb43d4237c44ff3896e5dd5
 T
 $src/test/java/HRPortalLoginTest.java,9/9/9950d281e7084d71acf8c39f15cb481e5d1b11ec
+N
+src/test/java/loginlogout.java,1/7/17a9c0aaf47c59230da14adac9e2dc8f58f0608c
--- src/test/java/HRPortalLoginTest.java
+++ src/test/java/HRPortalLoginTest.java
@@ -1,41 +1,67 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class HRPortalLoginTest {
-    private static final String EXCEL_PATH = "/Users/ramkoushikreddy/IdeaProjects/ECSAutomation/ExcelData/testDataLogin.xlsx";
+    private WebDriver driver;
+    private static final String EXCEL_PATH = "/Users/ramkoushikreddy/Downloads/testDataLogin.xlsx";
     private static final String LOGIN_URL = "http://ecs.memss.co.uk";
     private static final String EXPECTED_TITLE = "Dashboard | PixelsHR - All in One Cloud HR Software";
 
-    public static void main(String[] args) {
+    @BeforeClass
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+    }
 
-        try {
-            List<String[]> loginDataList = ExcelUtility.getAllLoginData(EXCEL_PATH);
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
 
-            for (String[] loginData : loginDataList) {
-                driver.get(LOGIN_URL);
-
-                LoginPage loginPage = new LoginPage(driver);
-                loginPage.setUsername(loginData[0]); // Set username
-                loginPage.setPassword(loginData[1]); // Set password
-                loginPage.clickLogin();
-
-                // Validate login by checking the page title
-                if (driver.getTitle().equals(EXPECTED_TITLE)) {
-                    System.out.println("Login successful for user: " + loginData[0]);
-                } else {
-                    System.out.println("Login failed for user: " + loginData[0]);
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            driver.quit();
+    @DataProvider(name = "loginData")
+    public Object[][] loginDataProvider() throws Exception {
+        List<String[]> allLoginData = ExcelUtility.getAllLoginData(EXCEL_PATH);
+        Object[][] data = new Object[allLoginData.size()][2];
+        int index = 0;
+        for (String[] loginData : allLoginData) {
+            data[index++] = loginData;
         }
+        return data;
+    }
+
+    @Test(dataProvider = "loginData")
+    public void testLoginLogout(String username, String password) {
+        driver.get(LOGIN_URL);
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.setUsername(username); // Set username
+        loginPage.setPassword(password); // Set password
+        loginPage.clickLogin();
+
+        // Validate login by checking the page title
+        if (driver.getTitle().equals(EXPECTED_TITLE)) {
+            System.out.println("Login successful for user: " + username);
+
+            // Perform Logout Operation
+            performLogout();
+
+            // Proceed to next credentials
+        } else {
+            System.out.println("Login failed for user: " + username);
+        }
+    }
+
+    private void performLogout() {
+        // Use the logout sequence from the provided script
+        driver.findElement(By.cssSelector(".d-xl-inline-block")).click();
+        driver.findElement(By.cssSelector(".text-dark")).click();
     }
 }
